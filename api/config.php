01bfab8ec56f5bf95341ffa9f65f85d61a142766
Lerior <leonardogarcia3500@gmail.com>
2024-11-01 22:04:07 -0600

    ahi va4

--- api/config.php
+++ api/config.php
@@ -1,15 +1,15 @@
 <?php
 class Config{
     const SECRET = "mindlandKEY";
-    
+    /*
     const HOST = "localhost";
     const BD = "mindland";
     const USER = "root";
     const PASS = "";
-    /*
-    const HOST = "sql100.infinityfree.com";
-    const BD = "if0_37599561_mindland";
-    const USER = "if0_37599561";
-    const PASS = "a7WTHUIchXMw0n4";
     */
+    const HOST = "mysql.railway.internal";
+    const BD = "mindland";
+    const USER = "root";
+    const PASS = "exbbxvPnUGJsCxqVXVPoqpnAakOrNnUx";
+    
 }
